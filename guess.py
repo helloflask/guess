7c65e0fe1fdf03d9f2e11f21f34d19b0e7e5f763
Hui Li <withlihui@gmail.com>
2016-11-01 18:49:57 +0800

    clear code

--- guess.py
+++ guess.py
@@ -8,7 +8,7 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'very hard to guess string'
-bootstrap = Bootstrap(app)  # 初始化Flask-Bootstap扩展
+bootstrap = Bootstrap(app)
 
 times = 10
 
@@ -16,7 +16,8 @@ times = 10
 @app.route('/')
 def index():
     global times
-    session['number'] = random.randint(0, 1000)  # 生成一个0~1000的随机数，存储到session变量里。
+    # generate a random number in 0~1000, store it into session.
+    session['number'] = random.randint(0, 1000)
     times = 10
     return render_template('index.html')
 
@@ -24,14 +25,14 @@ def index():
 @app.route('/guess', methods=['GET', 'POST'])
 def guess():
     global times
-    result = session.get('number')  # 从session变量里获取在index函数里生成的随机数。
+    result = session.get('number')
     form = GuessNumberForm()
     if form.validate_on_submit():
         times -= 1
         if times == 0:
             flash(u'你输了！')
             return redirect(url_for('.index'))
-        answer = form.number.data  # 获取表单的数据
+        answer = form.number.data
         if answer > result:
             flash(u'太大了！你还剩下%s次机会' % times)
         elif answer < result:
@@ -44,7 +45,6 @@ def guess():
 
 class GuessNumberForm(Form):
     number = IntegerField(u'输入数字：', validators=[
-        # 传入验证函数和相应的错误提示信息。
         Required(u'输入一个有效的数字！'),
         NumberRange(0, 1000, u'请输入0~1000以内的数字！')])
     submit = SubmitField(u'提交')
