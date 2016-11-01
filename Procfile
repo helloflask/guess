d6b699784235664faa6cdcd17e78c593cc05c48b
Hui Li <withlihui@gmail.com>
2016-11-01 22:46:05 +0800

    heroku config fix

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 -b 0.0.0.0:$PORT guess:app
+web: gunicorn -w 4 -b 0.0.0.0:5000 guess:app
--- guess.py
+++ guess.py
@@ -51,4 +51,4 @@ class GuessNumberForm(Form):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
